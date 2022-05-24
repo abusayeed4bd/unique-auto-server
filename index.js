7aad809138e4e92f83068a413f3cf4c542b147e2
abusayeed4bd <abusayeed4bd@gmail.com>
2022-05-25 03:13:54 +0600

    products api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,3 +17,35 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening unique auto parts port ${port}`)
 })
+
+
+
+// MONGO DB CONNECT CODE
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.oetir.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+
+        const productCollection = client.db('unique-auto').collection('products');
+
+        // product api
+        app.get('/products', async (req, res) => {
+            const products = await productCollection.find({}).toArray();
+            res.send(products);
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params;
+            filter = { id: (id) };
+            product = await productCollection.findOne(filter);
+            res.send(product)
+        })
+    } finally {
+
+    }
+}
+run().catch(console.dir);
